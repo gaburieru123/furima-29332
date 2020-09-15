0831310022c7ae4c0144ecd2b92d49263717a8f0
gaburieru123 <dari1tena@gmail.com>
2020-09-15 14:43:24 +0900

    マイグレーションファイル修正　null: false追加

--- db/migrate/20200915031539_add_password_to_users.rb
+++ db/migrate/20200915031539_add_password_to_users.rb
@@ -1,5 +1,6 @@
 class AddPasswordToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :encrypted_password_confirmation, :string
+    add_column :users, :encrypted_password_confirmation, :string, null: false
+
   end
 end
