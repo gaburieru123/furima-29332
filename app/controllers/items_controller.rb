744f037b30d6cb2495697bf7b0e0feefe1a40907
gaburieru123 <dari1tena@gmail.com>
2020-09-23 16:15:06 +0900

    destroyアクション　エラーハンドリング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :show]
 
   def index
@@ -20,8 +20,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
-
     if @item.update(item_params)
       redirect_to action: :show
     else
@@ -30,9 +28,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if item.destroy
+      redirect_to root_path
+    else
+      render 'show'
+    end
   end
 
   private
